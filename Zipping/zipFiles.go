08bf837a699d87c77fb458c00dace1fcdf07fb13
Benni Heiss <benni@inzing.eu>
2018-01-26 20:51:33 +0100

    init commit2

---
+++ Zipping/zipFiles.go
@@ -0,0 +1,54 @@
+package Zipping
+
+import (
+	"archive/zip"
+	"io"
+	"os"
+)
+
+func ZipFiles(filename string, files []string) error {
+
+	newFile, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer newFile.Close()
+
+	zipWriter := zip.NewWriter(newFile)
+	defer zipWriter.Close()
+
+	// Add files to zip
+	for _, file := range files {
+
+		zipfile, err := os.Open(file)
+		if err != nil {
+			return err
+		}
+		defer zipfile.Close()
+
+		// Get the file information
+		info, err := zipfile.Stat()
+		if err != nil {
+			return err
+		}
+
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+
+		// Change to deflate to gain better compression
+		// see http://golang.org/pkg/archive/zip/#pkg-constants
+		header.Method = zip.Deflate
+
+		writer, err := zipWriter.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+		_, err = io.Copy(writer, zipfile)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ ftp/ftpUpload.go
@@ -0,0 +1,52 @@
+package ftp
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/dutchcoders/goftp"
+)
+
+// UploadConf holds the Information needed to do a FTP Upload with UploadFile
+type UploadConf struct {
+	Host       string
+	UserName   string
+	Password   string
+	RemotePath string
+	FileName   string
+}
+
+//UploadFile Uploads the file with the given UploadConf via FTP
+func UploadFile(c UploadConf) (err error) {
+	var ftp *goftp.FTP
+
+	if ftp, err = goftp.Connect(c.Host); err != nil {
+		return err
+	}
+	defer ftp.Close()
+	//	fmt.Println("Connected To FTP")
+
+	if err = ftp.Login(c.UserName, c.Password); err != nil {
+		return err
+	}
+	//fmt.Println("Logged In")
+
+	//var curPath string
+	if _, err := ftp.Pwd(); err != nil {
+		return err
+	}
+	ftp.Cwd(c.RemotePath)
+	//fmt.Printf("Current path: %s \n", curPath)
+
+	var file *os.File
+	if file, err = os.Open(c.FileName); err != nil {
+		return err
+	}
+
+	if err := ftp.Stor(filepath.Base(c.FileName), file); err != nil {
+		return err
+	}
+	fmt.Println("finished upload: ", file.Name(), "to", c.Host)
+	return nil
+}
